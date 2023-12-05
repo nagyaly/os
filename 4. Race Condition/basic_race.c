7c9101cfce922fe862ad52ce12948d380804566c
Nagy K. Aly <nagy@aast.edu>
2023-12-05 13:11:37 +0200

    -m

--- 4. Race Condition/basic_race.c
+++ 4. Race Condition/basic_race.c
@@ -2,29 +2,32 @@
 #include <stdio.h>
 #include <unistd.h>
 int tickets = 10;
-void* thread1(void *args) {
+void* thread0(void *args) {
   while (1) {
     sleep(1);               // force delay
+    //====================== critical section start
     if(!tickets) break;
-    tickets--;              // critical section
+    tickets--;
     printf("Thread 1 sold 1 ticket, %d left\n", tickets);
+    //====================== critical section end
   }
   return NULL;
 }
-void* thread2(void *args) {
+void* thread1(void *args) {
   while (tickets > 0) {
     sleep(1);               // force delay
+    //====================== critical section start
     if(!tickets) break;
-    tickets--;              // critical section
+    tickets--;
     printf("Thread 2 sold 1 ticket, %d left\n", tickets);
+    //====================== critical section end
   }
   return NULL;
 }
 int main(void) {
-  pthread_t tid1, tid2;
-  int i;
+  pthread_t tid0, tid1;
+  pthread_create(&tid0, NULL, thread0, NULL);
   pthread_create(&tid1, NULL, thread1, NULL);
-  pthread_create(&tid2, NULL, thread2, NULL);
 
   pthread_exit(NULL); // wait for all threads to exit
   return 0;
--- 4. Race Condition/first_attempt.c
+++ 4. Race Condition/first_attempt.c
@@ -7,37 +7,36 @@ int turn = 0;
   each thread change turn to the other thread
   but the problem if on of the threads failed !
 */
-void* thread1(void *args) {
+void* thread0(void *args) {
   while (1) {
     sleep(1);               // force delay
     //====================== critical section start
     while(turn != 0);
     if(!tickets) break;
     tickets--;
+    printf("Thread 1 sold 1 ticket, %d left\n", tickets);
     turn = 1;
     //====================== critical section end
-    printf("Thread 1 sold 1 ticket, %d left\n", tickets);
   }
   return NULL;
 }
-void* thread2(void *args) {
+void* thread1(void *args) {
   while (1) {
     sleep(1);               // force delay
     //====================== critical section start
     while(turn != 1);
     if(!tickets) break;
     tickets--;
+    printf("Thread 2 sold 1 ticket, %d left\n", tickets);
     turn = 0;
     //====================== critical section end
-    printf("Thread 2 sold 1 ticket, %d left\n", tickets);
   }
   return NULL;
 }
 int main(void) {
-  pthread_t tid1, tid2;
+  pthread_t tid0, tid1;
+  pthread_create(&tid0, NULL, thread0, NULL);
   pthread_create(&tid1, NULL, thread1, NULL);
-  pthread_create(&tid2, NULL, thread2, NULL);
-
   pthread_exit(NULL); // wait for all threads to exit
   return 0;
 }
